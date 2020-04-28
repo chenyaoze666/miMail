938af173226cc28bad79189615246960527fef22
chenyaoze666 <1604275453@qq.com>
2020-04-28 11:00:28 +0800

    项目目录结构

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,12 @@
-module.exports = {
-  devServer:{
-    host:'localhost',
-    post:8080,
-    proxy:{
-      '/api':{
-        target:'代理地址',
-        changeOrigin:false//主机头url
-      }
-    }
-  }
-}
+// module.exports = {
+//   devServer:{
+//     host:'localhost',
+//     post:8080,
+//     proxy:{
+//       '/api':{
+//         target:'代理地址',
+//         changeOrigin:false//主机头url
+//       }
+//     }
+//   }
+// }
